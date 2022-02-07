5a1c0fdedc9976fc60252394a66f0de0f9319f31
Tyler Gandy <tylerbgandy@gmail.com>
2022-02-06 20:29:16 -0500

    added generateREADMe file to repo and began to write the first piece of code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -12,7 +12,47 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title},`;
+  return `# ${data.title},
+  ## Description
+  ${data.description}
+  
+  
+  ## Installation 
+  
+  
+  ## Usage
+  
+  
+  ## License 
+  
+  
+  
+  ## Contributing
+  
+  
+  ## Tests
+  
+  
+  
+  ## Questions
+  
+  
+  ## Up Next
+  
+  For a closer preview of what we explore in each unit of the boot camp, dive into each unit’s overview to learn the following:
+  
+  * What you will learn in the unit and why it’s important
+  
+  * How topics in the unit relate to what came earlier in the boot camp
+  
+  * Activities you’ll complete and what skills they focus on
+  
+  * Common interview questions related to that week’s material
+  
+  * Helpful resources if you want to get ahead or gain more experience after the unit
+  
+  © 2021 Tyler B Gandy. Confidential and Proprietary. All Rights Reserved.
+  `;
 }
 
 module.exports = generateMarkdown;
